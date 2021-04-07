b7c3c95d5ea6721f6d04ab858b18363bec02ba02
iiPython <ben@iipython.cf>
2021-04-07 13:01:45 -0500

    Fix the stupid command loader

--- sprint/core/executor/loader.py
+++ sprint/core/executor/loader.py
@@ -2,7 +2,10 @@
 # handles command loading
 
 # Modules
+import os
+import json
 from ..handler.logging import Logger
+from os.path import isdir, isfile, join
 
 # Main loader class
 class Loader(object):
@@ -14,4 +17,77 @@ class Loader(object):
             self.logger.warn("sprint path is empty, no commands will be loaded.")
 
     def locate_command(self, name: str):
-        return
+
+        # Convert argument to string
+        name = repr(name)
+
+        # Loop through path
+        MATCHING = []
+        for PATH_DIR in self.path:
+            if not isdir(PATH_DIR):
+                self.logger.warn(f"PATH directory '{PATH_DIR}' does not exist, skipped.")
+                continue
+
+            # Loop through publishers
+            for PUBLISHER in os.listdir(PATH_DIR):
+                PUB_PATH = join(PATH_DIR, PUBLISHER)
+
+                # Handle metadata
+                author = ("unknown", "unknown")
+                metafile = join(PUB_PATH, "METADATA")
+
+                if isfile(metafile):
+                    try:
+                        with open(metafile, "r") as file:
+                            meta = file.read()
+
+                        meta = json.loads(meta)
+                        author = (meta["author"], meta["author_contact"])
+
+                    except json.JSONDecodeError:
+                        pass
+
+                # Loop through modules
+                for MODULE in os.listdir(PUB_PATH):
+                    MOD_PATH = join(PUB_PATH, MODULE)
+
+                    if not isdir(MOD_PATH):
+                        continue  # Skip METADATA and asset files (READMEs, etc)
+
+                    # Loop through commands
+                    for COMMAND in os.listdir(MOD_PATH):
+                        CMD_PATH = join(MOD_PATH, COMMAND)
+
+                        # Check valdiity
+                        if not isfile(CMD_PATH):
+                            continue  # Skip asset folders
+
+                        # Grab data
+                        cmd_name = COMMAND.split("\\")[-1].split(".")[0]
+
+                        # Check name
+                        if cmd_name != name:
+                            continue
+
+                        # Take note
+                        MATCHING.append({
+                            "command": {
+                                "name": cmd_name,
+                                "path": CMD_PATH,
+                                "full_name": author[0] + "." + cmd_name
+                            },
+                            "author": author,
+                            "module": {
+                                "name": MODULE,
+                                "path": MOD_PATH
+                            }
+                        })
+
+        # Handle conflicts
+        STATUS = False
+        if len(MATCHING) > 1:
+            STATUS = True
+            MATCHING = [_["command"]["full_name"] for _ in MATCHING]
+
+        # Return matching commands
+        return STATUS, MATCHING
--- sprint/core/executor/wrapper.py
+++ sprint/core/executor/wrapper.py
@@ -1,12 +1,14 @@
 # Modules
 from .loader import Loader
+from ..handler.logging import Logger
 from ..handler.params import SprintParams
 
 # Load path
 from ...utils.path import PATH
 
-# Initialize loader
+# Initialization
 LOADER = Loader(PATH)
+LOGGER = Logger()
 
 # Main command class
 class Command(object):
@@ -16,6 +18,24 @@ class Command(object):
         self.params = params
 
     def execute(self):
+
+        # Grab command
+        status, command = LOADER.locate_command(self.cmd)
+        if status is False:
+            if not command:
+                return LOGGER.error(f"command '{self.cmd}' not found.")
+
+        else:
+            print("Command conflict found, below are your options:")
+            for match in command:
+                print(match)
+
+            return
+
+        command = command[0]
+        location = command["command"]["path"]
+
+        print(command, location)
 
         # Check
         return True
